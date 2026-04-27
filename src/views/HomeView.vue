8931c95775fd02f5e440b94fb7502438fadeb05d
maysazvn <maysabmachado84@gmail.com>
2026-04-27 16:52:33 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <span><a href="#">EL BANCO</a></span>
+    <p>Apreço a leitura</p>
+
+    <input type="text"> <i class="fa-solid fa-magnifying-glass"></i>
+
+    <ul>
+      <li>
+        Termos
+      </li>
+      <li>
+        Equipe
+      </li>
+      <li>
+        Envio
+      </li>
+      <li>
+        Devoluções
+      </li>
+    </ul>
+<ul>
+  <li><i class="fa-solid fa-cart-shopping"></i></li>
+  <li><i class="fa-solid fa-heart"></i></li>
+  <li> <i class="fa-solid fa-user"></i></li>
+</ul>
+  </header>
+
+  <main>
+   <section>
+    <p>Livro destaque</p>
+
+    <h2>blablabla</h2>
+    <p></p>
+    <button>Acessar página do livro</button>
+
+    <img src="" alt="">
+   </section>
+  </main>
+
+  <footer>
+
+  </footer>
+</template>
